beeb8eaecbc8626dd1fddd50bd9027704b692855
Brunno2024 <167224923+Brunno2024@users.noreply.github.com>
2024-04-24 00:09:10 -0300

    Update Prueba audios.js

--- plugins/Prueba audios.js
+++ plugins/Prueba audios.js
@@ -16,3 +16,5 @@ if (!db.data.chats[m.chat].audios) return;
 const vn = './media/grrr.mp3';
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendMessage(m.chat, { audio: { url: vn }, fileName: 'error.mp3', mimetype: 'audio/mp4', ptt: true }, { quoted: m })} 
+return !0 }
+export default handler
